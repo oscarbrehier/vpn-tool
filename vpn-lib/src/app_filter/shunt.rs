047b080784f659904e3a0acc406ba031ca982519
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2026-03-05 22:04:30 +0100

    handle outbound and inbound packet methods

--- vpn-lib/src/app_filter/shunt.rs
+++ vpn-lib/src/app_filter/shunt.rs
@@ -1,9 +1,97 @@
-use std::net::Ipv4Addr;
+use std::{collections::HashMap, net::Ipv4Addr};
 
 use anyhow::Ok;
 use etherparse::SlicedPacket;
 use tokio::sync::mpsc;
 use windivert::{layer::NetworkLayer, packet::WinDivertPacket};
+
+type ConnectionMap = HashMap<u16, Ipv4Addr>;
+
+fn handle_outbound(
+    packet: &mut WinDivertPacket<'_>,
+    virtual_ip: Ipv4Addr,
+    interface_i: u32,
+    connections: &mut ConnectionMap,
+) -> Result<(), String> {
+    let data = &mut packet.data;
+
+    if data.len() < 20 {
+        return Err("Packet too short".into());
+    }
+
+    let version = data[0] >> 4;
+    if version != 4 {
+        return Err("IPv6 is not supporteed".into());
+    }
+
+    let original_ip = Ipv4Addr::from([data[12], data[13], data[14], data[15]]);
+
+    let ip_bytes = virtual_ip.octets();
+    data[12..16].copy_from_slice(&ip_bytes);
+
+    let transport_header_start = 20;
+
+    if data.len() < transport_header_start + 2 {
+        return Err("Packet too short for transport header".into());
+    }
+
+    let source_port = u16::from_be_bytes([
+        data[transport_header_start],
+        data[transport_header_start + 1],
+    ]);
+
+    connections.insert(source_port, original_ip);
+
+    packet.address.if_idx = interface_i;
+    packet.address.set_outbound(true);
+
+    packet.recalculate_checksums()?;
+
+    Ok(())
+}
+
+fn handle_inbound(
+    packet: &mut WinDivertPacket<'_>,
+    connections: &ConnectionMap,
+) -> Result<(), String> {
+    let data = &mut packet.data;
+
+    if data.len() < 20 {
+        return Err("Packet too short".into());
+    }
+
+    let version = data[0] >> 4;
+    if version != 4 {
+        return Err("IPv6 is not supported".to_string());
+    }
+
+    let transport_header_start = 20;
+
+    if data.len() < transport_header_start + 2 {
+        return Err("Packet too short for transport header".into());
+    }
+
+    let dest = u16::from_be_bytes([
+        data[transport_header_start],
+        data[transport_header_start + 1],
+    ]);
+
+    if let Some(original_ip) = connections.get(&dest) {
+        
+		let ip_bytes = virtual_ip.octets();
+        data[12..16].copy_from_slice(&ip_bytes);
+
+		packet.address.if_idx = 0;
+		packet.address.set_outbound(false);
+
+		packet.recalculate_checksums()?;
+
+    } else {
+        return Err("Application not found with specified port".into());
+    }
+
+    Ok(())
+}
 
 fn build_filter_string(pids: &[u32]) -> String {
     if pids.is_empty() {
@@ -18,38 +106,16 @@ fn build_filter_string(pids: &[u32]) -> String {
     format!("outbound and ({})", pid_conditions.join(" or "))
 }
 
-fn redirect_to_tunnel(
-    packet: &mut WinDivertPacket<'_>,
-    virtual_ip: Ipv4Addr,
-    interface_i: u32,
-) -> Result<(), String> {
-    let data = &mut packet.data;
-
-    if data.len() < 20 {
-        return Err("Packet too short".into());
-    }
-
-    let version = data[0] >> 4;
-
-    if version == 4 {
-        let ip_bytes = virtual_ip.octets();
-        data[12..16].copy_from_slice(&ip_bytes);
-
-        packet.address.if_idx = interface_i;
-        packet.address.set_outbound(true);
-    } else {
-        return Err("IPv6 is not supported".to_string());
-    }
-
-    Ok(())
-}
-
 #[cfg(target_os = "windows")]
 pub async fn start_packet_redirection(
     initial_pids: Vec<u32>,
     mut filter_rx: mpsc::UnboundedReceiver<Vec<u32>>,
+	virtual_ip: Ipv4Addr,
+	interface_i: u32
 ) -> Result<(), String> {
     use windivert::{WinDivert, prelude::WinDivertFlags};
+
+	let mut connections = ConnectionMap::new();
 
     let current_filter = build_filter_string(&initial_pids);
 
