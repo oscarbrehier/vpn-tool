aec4d0830c57b8a903182b4749b358d54e46ed63
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2026-03-07 19:18:21 +0100

    filter out noise from runnning apps

--- vpn-lib/src/app_filter/scanner.rs
+++ vpn-lib/src/app_filter/scanner.rs
@@ -1,35 +1,44 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf};
 
-use serde::{Serialize};
+use serde::Serialize;
 use sysinfo::{System};
 
-#[derive(Debug, Serialize)]
-pub struct AppProcess {
-    pub pid: u32,
+#[derive(Debug, Serialize, Clone)]
+pub struct AppGroup {
+    pub pids: Vec<u32>,
     pub name: String,
     pub path: Option<PathBuf>,
     pub icon_base64: Option<String>,
 }
 
-pub fn get_running_apps() -> Result<Vec<AppProcess>, String> {
+pub fn get_running_apps() -> Result<Vec<AppGroup>, String> {
     let mut sys = System::new_all();
     sys.refresh_all();
 
-    let mut processes: Vec<AppProcess> = Vec::new();
+    let mut grouped_processes: HashMap<String, AppGroup> = HashMap::new();
 
     for (pid, process) in sys.processes() {
+        if let Some(path) = process.exe() {
+            let path_str = path.to_string_lossy();
 
-		let pid_val = pid.as_u32();
-		let name = process.name().to_string_lossy().into_owned();
-		let path = process.exe().map(|p| p.to_path_buf());
+            let is_user_app = path_str.contains("Program Files")
+                || path_str.contains("AppData")
+                || path_str.contains("Users");
 
-        processes.push(AppProcess {
-            pid: pid_val,
-            name: name,
-            path: path,
-            icon_base64: None,
-        });
+            if is_user_app {
+                let name = process.name().to_string_lossy().replace(".exe", "");
+
+                let entry = grouped_processes.entry(name.clone()).or_insert(AppGroup {
+                    name: name,
+                    path: Some(path.to_path_buf()),
+                    icon_base64: None,
+                    pids: Vec::new(),
+                });
+
+                entry.pids.push(pid.as_u32());
+            }
+        }
     }
 
-    Ok(processes)
+    Ok(grouped_processes.values().cloned().collect())
 }
